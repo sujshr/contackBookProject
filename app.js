0c4e7f85a93301d725e9236fd04b2ae181ade593
Sujal Shresth <sujshrest@example.com>
2023-12-10 15:18:41 +0530

    Assignment Started

---
+++ app.js
@@ -0,0 +1,5 @@
+// create a function and following the progressions inside the function.
+
+// Progression 1: Create a promise call which fetches data
+// Progression 2: Display the fetched data in the form of list
+// Progression 3: When the promise call is rejected then throw an error
